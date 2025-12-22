f669712da10d77b8804e092091f7a45369f96fce
GrannyDev <bastien.barbe65@gmail.com>
2025-12-22 20:40:04 +0100

    :sparkles: new json format

--- JSONDumper.cpp
+++ JSONDumper.cpp
@@ -37,10 +37,12 @@ void JSONDumper::Dump() {
     // adders
     unsigned int adderNb = 0;
     unsigned int bitPos = 0;
+    unsigned int totalNbMuxes = 0;
+    std::vector<unsigned int> muxNbToMuxBw;
     
     for (const auto& layer : layers_) {
         for (const auto& adder : layer.adders) {
-            outputFile_ << std::string(nbTabs, '\t') << "\"Adder_" << adderNb << "\": {\n";
+            outputFile_ << std::string(nbTabs, '\t') << "\"ADDER_" << adderNb << "\": {\n";
             nbTabs++;
 
             // Get parameter indices for this adder
@@ -62,30 +64,40 @@ void JSONDumper::Dump() {
             // Process each parameter
             unsigned int paramIdx = 0;
             for (const auto& param : adder.variables) {
-                unsigned int paramGlobalIdx = paramGlobalIdxOffset + paramIdx;
-                VariableDefs currentParamType = idxToVarMap_.at(paramIdx);
+                const unsigned int paramGlobalIdx = paramGlobalIdxOffset + paramIdx;
+                const VariableDefs currentParamType = idxToVarMap_.at(paramIdx);
                 const unsigned int computedBW = solutionNode_.variableBitWidths[paramGlobalIdx];
-
-                // Skip non-shift and non-adder parameters
-                if (currentParamType == VariableDefs::LEFT_INPUTS || 
-                    currentParamType == VariableDefs::RIGHT_INPUTS) {
-                    paramIdx++;
-                    bitPos += param.possibleValuesFusion.size();
-                    continue;
-                }
 
                 // Collect selected shifts and find max shift
                 std::vector<int> selectedShifts;
+                std::vector<std::string> inputs;
                 unsigned int maxShift = 0;
                 for (const int v : param.possibleValuesFusion) {
                     if (solutionNode_.rscm.set.test(bitPos + v + param.zeroPoint)) {
-                        selectedShifts.push_back(v);
-                        if (currentParamType == VariableDefs::OUTPUTS_SHIFTS) {
+                        if (currentParamType == VariableDefs::OUTPUTS_SHIFTS)
+                        {
+                            selectedShifts.push_back(v);
                             // Output shift uses power of 2, but only count if v > 0
                             if (v > 0) {
                                 maxShift = std::max(maxShift, static_cast<unsigned int>(1 << v));
                             }
+                        } else if (currentParamType == VariableDefs::LEFT_INPUTS || currentParamType == VariableDefs::RIGHT_INPUTS) {
+                            if (v < 0) {
+                                if (v == -1) {
+                                    inputs.emplace_back("X");
+                                    selectedShifts.push_back(0);
+                                } else {
+                                    unsigned int shiftAmount = std::abs(v + 1);
+                                    inputs.emplace_back("X");
+                                    selectedShifts.push_back(static_cast<int>(shiftAmount));
+                                    maxShift = std::max(maxShift, shiftAmount);
+                                }
+                            } else {
+                                inputs.push_back("ADDER_" + std::to_string(v));
+                                selectedShifts.push_back(0);
+                            }
                         } else {
+                            selectedShifts.push_back(v);
                             // Input shift uses direct value
                             maxShift = std::max(maxShift, static_cast<unsigned int>(v));
                         }
@@ -94,6 +106,11 @@ void JSONDumper::Dump() {
 
                 // Check if this is a mux (more than one option selected)
                 bool isMux = selectedShifts.size() > 1;
+                if (isMux)
+                {
+                    muxNbToMuxBw.push_back(std::ceil(std::log2(selectedShifts.size())));
+                    totalNbMuxes++;
+                }
                 if (currentParamType == VariableDefs::RIGHT_MULTIPLIER) {
                     // For ADD_SUB, it's a mux if it can do both add and subtract
                     int addCount = 0, subCount = 0;
@@ -101,7 +118,7 @@ void JSONDumper::Dump() {
                         if (v == -1) subCount++;
                         else if (v == 1) addCount++;
                     }
-                    isMux = (addCount > 0 && subCount > 0);
+                    isMux = addCount > 0 && subCount > 0;
                 }
 
                 // Get theoretical bitwidth
@@ -121,15 +138,30 @@ void JSONDumper::Dump() {
                     unsigned int leftShiftBW = GetParamBitWidth(VariableDefs::LEFT_SHIFTS, adderNb);
                     unsigned int rightShiftBW = GetParamBitWidth(VariableDefs::RIGHT_SHIFTS, adderNb);
                     theoreticalBW = std::max(leftShiftBW, rightShiftBW) + 1;
+                } else
+                {
+                    theoreticalBW = computedBW; // For inputs, theoretical equals computed
                 }
 
                 // Output parameter information
                 outputFile_ << std::string(nbTabs, '\t') << "\"" << varDefsTostring_(currentParamType) << "\": {\n";
                 nbTabs++;
 
+                if (currentParamType == VariableDefs::LEFT_INPUTS || currentParamType == VariableDefs::RIGHT_INPUTS)
+                {
+                    // add the inputs values
+                    outputFile_ << std::string(nbTabs, '\t') << "\"inputs\": [";
+                    bool first = true;
+                    for (const std::string& input : inputs) {
+                        if (!first) outputFile_ << ", ";
+                        outputFile_ << "\"" << input << "\"";
+                        first = false;
+                    }
+                    outputFile_ << "],\n";
+                }
                 if (currentParamType == VariableDefs::RIGHT_MULTIPLIER) {
                     // Handle ADD_SUB specially
-                    outputFile_ << std::string(nbTabs, '\t') << "\"type\": \"";
+                    outputFile_ << std::string(nbTabs, '\t') << R"("type": ")";
                     
                     // Determine if it's add, subtract, or both
                     int addCount = 0, subCount = 0;
@@ -146,13 +178,6 @@ void JSONDumper::Dump() {
                         outputFile_ << "adder";
                     }
                     outputFile_ << "\",\n";
-                    
-                    outputFile_ << std::string(nbTabs, '\t') << "\"theoretical_bitwidth\": " << theoreticalBW << ",\n";
-                    outputFile_ << std::string(nbTabs, '\t') << "\"computed_bitwidth\": " << computedBW;
-                    if (isMux) {
-                        outputFile_ << ",\n" << std::string(nbTabs, '\t') << "\"mux_nb\": " << paramIdx;
-                    }
-                    outputFile_ << "\n";
                 } else {
                     // Handle shift parameters
                     outputFile_ << std::string(nbTabs, '\t') << "\"shifts\": [";
@@ -164,14 +189,13 @@ void JSONDumper::Dump() {
                         first = false;
                     }
                     outputFile_ << "],\n";
-                    
-                    outputFile_ << std::string(nbTabs, '\t') << "\"theoretical_wire_length\": " << theoreticalBW << ",\n";
-                    outputFile_ << std::string(nbTabs, '\t') << "\"computed_wire_length\": " << computedBW;
-                    if (isMux) {
-                        outputFile_ << ",\n" << std::string(nbTabs, '\t') << "\"mux_nb\": " << paramIdx;
-                    }
-                    outputFile_ << "\n";
                 }
+                outputFile_ << std::string(nbTabs, '\t') << "\"theoretical_bitwidth\": " << theoreticalBW << ",\n";
+                outputFile_ << std::string(nbTabs, '\t') << "\"computed_bitwidth\": " << computedBW;
+                if (isMux) {
+                    outputFile_ << ",\n" << std::string(nbTabs, '\t') << "\"mux_nb\": " << totalNbMuxes - 1;
+                }
+                outputFile_ << "\n";
 
                 nbTabs--;
                 outputFile_ << std::string(nbTabs, '\t') << "}";
@@ -191,19 +215,12 @@ void JSONDumper::Dump() {
         }
     }
 
-    outputFile_ << std::string(nbTabs, '\t') << "\"RSCM\": {\n";
-    nbTabs++;
-    
-    // Reset for second pass to generate RSCM top module
-    adderNb = 0;
-    bitPos = 0;
-    
     // First, collect mux encoding information and track mux assignments
     // This logic is copied from Solver::SolveConfigToMuxMapping()
     std::vector<std::vector<unsigned int>> muxEncoding(targets_.size());
     unsigned int bitIndex = 0;
     unsigned int currentAdder = 0;
-    
+
     for (const auto& layer : layers_) {
         for (const auto& adder : layer.adders) {
             unsigned int paramIdx = 0;
@@ -215,7 +232,7 @@ void JSONDumper::Dump() {
                         nbPossibleValues++;
                     }
                 }
-                
+
                 // If more than one value, we have a mux
                 if (nbPossibleValues > 1) {
                     unsigned int valueNumber = 0;
@@ -237,148 +254,53 @@ void JSONDumper::Dump() {
             currentAdder++;
         }
     }
-    
-    // Now generate the adder information
-    adderNb = 0;
-    bitPos = 0;
 
-    for (const auto& layer : layers_) {
-        for (const auto& adder : layer.adders) {
-            outputFile_ << std::string(nbTabs, '\t') << "\"ADDER_" << adderNb << "\": {\n";
-            nbTabs++;
-            
-            // Get the left and right input information
-            std::vector<std::string> leftInputs;
-            std::vector<unsigned int> leftShifts;
-            std::vector<std::string> rightInputs;
-            std::vector<unsigned int> rightShifts;
-            unsigned int maxShiftLeft = 0;
-            unsigned int maxShiftRight = 0;
-            int leftInputMuxNb = -1;
-            int rightInputMuxNb = -1;
-            
-            unsigned int paramIdx = 0;
-            for (const auto& param : adder.variables) {
-                VariableDefs paramType = idxToVarMap_.at(paramIdx);
-                
-                if (paramType == VariableDefs::LEFT_INPUTS || paramType == VariableDefs::RIGHT_INPUTS) {
-                    std::vector<std::string>& inputs = (paramType == VariableDefs::LEFT_INPUTS) ? leftInputs : rightInputs;
-                    std::vector<unsigned int>& shifts = (paramType == VariableDefs::LEFT_INPUTS) ? leftShifts : rightShifts;
-                    unsigned int& maxShift = (paramType == VariableDefs::LEFT_INPUTS) ? maxShiftLeft : maxShiftRight;
-                    // Check if this parameter is a mux
-                    unsigned int bitsSet = 0;
-                    int& inputMuxNb = (paramType == VariableDefs::LEFT_INPUTS) ? leftInputMuxNb : rightInputMuxNb;
-                    
-                    // Collect inputs with embedded shifts
-                    for (const int v : param.possibleValuesFusion) {
-                        if (solutionNode_.rscm.set.test(bitPos + v + param.zeroPoint)) {
-                            bitsSet++;
-                            if (v < 0) {
-                                if (v == -1) {
-                                    inputs.emplace_back("X");
-                                    shifts.push_back(0);
-                                } else {
-                                    unsigned int shiftAmount = std::abs(v + 1);
-                                    inputs.emplace_back("X");
-                                    shifts.push_back(shiftAmount);
-                                    maxShift = std::max(maxShift, shiftAmount);
-                                }
-                            } else {
-                                inputs.push_back("ADDER_" + std::to_string(v));
-                                shifts.push_back(0);
-                            }
-                        }
-                    }
-                    maxShift += inputBW_;
-                    if (bitsSet > 1) {
-                        inputMuxNb = static_cast<int>(paramIdx);
-                    }
-                }
-                
-                bitPos += param.possibleValuesFusion.size();
-                paramIdx++;
-            }
-            
-            // Output left inputs
-            outputFile_ << std::string(nbTabs, '\t') << "\"left_inputs\": [";
-            for (size_t i = 0; i < leftInputs.size(); ++i) {
-                if (i > 0) outputFile_ << ", ";
-                outputFile_ << "\"" << leftInputs[i] << "\"";
-            }
-            outputFile_ << "],\n";
-            
-            outputFile_ << std::string(nbTabs, '\t') << "\"left_shifts\": [";
-            for (size_t i = 0; i < leftShifts.size(); ++i) {
-                if (i > 0) outputFile_ << ", ";
-                outputFile_ << leftShifts[i];
-            }
-            outputFile_ << "],\n";
-            
-            // Add mux_nb if there are multiple left inputs
-            if (leftInputMuxNb >= 0) {
-                outputFile_ << std::string(nbTabs, '\t') << "\"left_mux_nb\": " << leftInputMuxNb << ",\n";
-            }
-            
-            // Output right inputs
-            outputFile_ << std::string(nbTabs, '\t') << "\"right_inputs\": [";
-            for (size_t i = 0; i < rightInputs.size(); ++i) {
-                if (i > 0) outputFile_ << ", ";
-                outputFile_ << "\"" << rightInputs[i] << "\"";
-            }
-            outputFile_ << "],\n";
-            
-            outputFile_ << std::string(nbTabs, '\t') << "\"right_shifts\": [";
-            for (size_t i = 0; i < rightShifts.size(); ++i) {
-                if (i > 0) outputFile_ << ", ";
-                outputFile_ << rightShifts[i];
-            }
-            outputFile_ << "],\n";
-            
-            // Add mux_nb if there are multiple right inputs
-            if (rightInputMuxNb >= 0) {
-                outputFile_ << std::string(nbTabs, '\t') << "\"right_mux_nb\": " << rightInputMuxNb << ",\n";
-            }
-            
-            // Compute theoretical and computed bitwidths for left and right
-            unsigned int leftComputedBW = GetParamBitWidth(VariableDefs::LEFT_INPUTS, adderNb);
-            unsigned int rightComputedBW = GetParamBitWidth(VariableDefs::RIGHT_INPUTS, adderNb);
-            maxShiftLeft = maxShiftLeft == inputBW_ ? leftComputedBW : maxShiftLeft;
-            maxShiftRight = maxShiftRight == inputBW_ ? rightComputedBW : maxShiftRight;
-            
-            outputFile_ << std::string(nbTabs, '\t') << "\"left_theoretical_bitwidth\": " << maxShiftLeft << ",\n";
-            outputFile_ << std::string(nbTabs, '\t') << "\"left_computed_bitwidth\": " << leftComputedBW << ",\n";
-            outputFile_ << std::string(nbTabs, '\t') << "\"right_theoretical_bitwidth\": " << maxShiftRight << ",\n";
-            outputFile_ << std::string(nbTabs, '\t') << "\"right_computed_bitwidth\": " << rightComputedBW << "\n";
-            
-            nbTabs--;
-            outputFile_ << std::string(nbTabs, '\t') << "},\n";
-            adderNb++;
-        }
-    }
-    
-    outputFile_ << std::string(nbTabs, '\t') << "\"output\": \"ADDER_" << (GetTotalAdderCount() - 1) << "\",\n";
-    
-    // Output encoding for each target
-    outputFile_ << std::string(nbTabs, '\t') << "\"encoding\": {\n";
+    // metadata
+    outputFile_ << std::string(nbTabs, '\t') << "\"meta\": {\n";
     nbTabs++;
-    for (size_t i = 0; i < targets_.size(); i++) {
-        outputFile_ << std::string(nbTabs, '\t') << "\"" << scmDesigns_[i].first << "\": [";
-        for (size_t j = 0; j < muxEncoding[i].size(); j++) {
-            if (j > 0) outputFile_ << ", ";
-            outputFile_ << muxEncoding[i][j];
+    outputFile_ << std::string(nbTabs, '\t') << "\"wIn\": " << inputBW_ << ",\n";
+    unsigned int wConf = std::floor(std::log2(targets_.size() + 1));
+    unsigned int wMuxTotal = 0;
+    for (const auto& bw : muxNbToMuxBw) {
+        wMuxTotal += bw;
+    }
+    outputFile_ << std::string(nbTabs, '\t') << "\"wConf\": " << wConf << ",\n";
+    outputFile_ << std::string(nbTabs, '\t') << "\"wSelec\": " << wMuxTotal << ",\n";
+    outputFile_ << std::string(nbTabs, '\t') << "\"needs_table\": " << (wConf != wMuxTotal ? "true" : "false") << ",\n";
+    outputFile_ << std::string(nbTabs, '\t') << "\"wOut\": " << GetParamBitWidth(VariableDefs::OUTPUTS_SHIFTS, GetTotalAdderCount() - 1) << ",\n";
+    outputFile_ << std::string(nbTabs, '\t') << R"("output": "ADDER_)" << GetTotalAdderCount() - 1 << "\",\n";
+    outputFile_ << std::string(nbTabs, '\t') << "\"muxes_bw\": [";
+    for (size_t i = 0; i < muxNbToMuxBw.size(); i++) {
+        if (i > 0) outputFile_ << ", ";
+        outputFile_ << muxNbToMuxBw[i];
+    }
+    outputFile_ << "],\n";
+    outputFile_ << std::string(nbTabs, '\t') << "\"constants\": [";
+    for (int i = 0; i < targets_.size(); i++) {
+        if (i > 0) outputFile_ << ", ";
+        outputFile_ << scmDesigns_[i].first;
+    }
+    outputFile_ << "],\n";
+
+    // Output encoding for each target
+    outputFile_ << std::string(nbTabs, '\t') << R"("encoding": [)" << "\n";
+    nbTabs++;
+    for (size_t targetIndex = 0; targetIndex < targets_.size(); targetIndex++) {
+        outputFile_ << std::string(nbTabs, '\t') << "[";
+        for (size_t i = 0; i < muxEncoding[targetIndex].size(); i++) {
+            if (i > 0) outputFile_ << ", ";
+            outputFile_ << muxEncoding[targetIndex][i];
         }
         outputFile_ << "]";
-        if (i < targets_.size() - 1) {
+        if (targetIndex < targets_.size() - 1) {
             outputFile_ << ",";
         }
         outputFile_ << "\n";
     }
     nbTabs--;
-    outputFile_ << std::string(nbTabs, '\t') << "}\n";
-    
+    outputFile_ << std::string(nbTabs, '\t') << "]\n";
     nbTabs--;
     outputFile_ << std::string(nbTabs, '\t') << "}\n";
-
     outputFile_ << "}\n";
 }
 
